2ef43bb928f01d92dc4beedf090ba1f945eebf39
Philip Tunbjer <philip.tunbjer@gmail.com>
2022-01-23 02:20:42 +0100

    Preliminary renditions of Player and Collectible classes

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,40 @@
 class Player {
   constructor({x, y, score, id}) {
-
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
   }
 
   movePlayer(dir, speed) {
-
+    switch (dir){
+      case 'up':
+        this.y = this.y - speed;
+        break;
+      case 'down':
+        this.y = this.y + speed;
+        break;
+      case 'left':
+        this.x = this.x - speed;
+        break;
+      case 'right':
+        this.x = this.x + speed;
+        break;
+    }
   }
 
   collision(item) {
-
+    if (this.x == item.x && this.y == item.y){
+      return true;
+    }
+    return false;
   }
 
   calculateRank(arr) {
-
+    arr.sort((a,b) => a.score > b.score);
+    currentRank = arr.indexOf(this.id) + 1;
+    totalPlayers = arr.length
+    return `Rank: ${currentRank}/${totalPlayers}`
   }
 }
 
